fda59442127e3ed9f1d50b1c0e9d9a985dd7a9e9
chansung park <deep.diver.csp@gmail.com>
2023-01-17 13:51:01 +0000

    remove constants.py. constants are defined in module level

--- constants.py
+++
@@ -1,6 +0,0 @@
-# The padding token and maximum prompt length are specific to the text encoder.
-# If you're using a different text encoder be sure to change them accordingly.
-PADDING_TOKEN = 49407
-MAX_PROMPT_LENGTH = 77
-
-RESOLUTION = 512
