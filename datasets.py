3364951dc8cff7c655d3f90188af9b02a0033d3e
chansung park <deep.diver.csp@gmail.com>
2023-01-17 13:49:50 +0000

    refactor dataset preparation functions as class

--- datasets.py
+++ datasets.py
@@ -1,3 +1,5 @@
+from typing import Dict, Tuple, Callable
+
 import logging
 import itertools
 
@@ -7,180 +9,208 @@ import keras_cv
 from imutils import paths
 from keras_cv.models.stable_diffusion.clip_tokenizer import SimpleTokenizer
 from keras_cv.models.stable_diffusion.text_encoder import TextEncoder
-from constants import PADDING_TOKEN, MAX_PROMPT_LENGTH, RESOLUTION
 
+PADDING_TOKEN = 49407
+MAX_PROMPT_LENGTH = 77
+POS_IDS = tf.convert_to_tensor([list(range(MAX_PROMPT_LENGTH))], dtype=tf.int32)
 AUTO = tf.data.AUTOTUNE
 
-augmenter = keras_cv.layers.Augmenter(
-    layers=[
-        keras_cv.layers.CenterCrop(RESOLUTION, RESOLUTION),
-        keras_cv.layers.RandomFlip(),
-        tf.keras.layers.Rescaling(scale=1.0 / 127.5, offset=-1),
-    ]
-)
+class DatasetUtils:
+    def __init__(
+        self,
+        instance_images_url: str,
+        class_images_url: str,
+        unique_id: str, 
+        class_category: str,
+        img_height: int = 512,
+        img_width: int = 512,
+        batch_size: int = 1
+    ):
 
-def process_text(caption):
-    """tokenize the caption"""
+        self.instance_images_url = instance_images_url
+        self.class_images_url = class_images_url
+        self.unique_id = unique_id
+        self.class_category = class_category
+        self.img_height = img_height
+        self.img_width = img_width
+        self.batch_size = batch_size
 
-    tokenizer = SimpleTokenizer()
+        self.tokenizer = SimpleTokenizer()
+        self.text_encoder = TextEncoder(MAX_PROMPT_LENGTH)
 
-    tokens = tokenizer.encode(caption)
-    tokens = tokens + [PADDING_TOKEN] * (MAX_PROMPT_LENGTH - len(tokens))
-    return np.array(tokens)
+        self.augmenter = keras_cv.layers.Augmenter(
+            layers=[
+                keras_cv.layers.CenterCrop(self.img_height, self.img_width),
+                keras_cv.layers.RandomFlip(),
+                tf.keras.layers.Rescaling(scale=1.0 / 127.5, offset=-1),
+            ]
+        )
 
-def get_captions(
-    num_instance_images, num_class_images,
-    unique_id, class_category
-):
-    """prepare captions for instance and class images"""
+    def _process_text(self, caption) -> np.ndarray::
+        """tokenize the caption"""
 
-    instance_caption = f"a photo of {unique_id} {class_category}"
-    instance_captions = [instance_caption] * num_instance_images
+        tokens = self.tokenizer.encode(caption)
+        tokens = tokens + [PADDING_TOKEN] * (MAX_PROMPT_LENGTH - len(tokens))
+        return np.array(tokens)
 
-    class_caption = f"a photo of {class_category}"
-    class_captions = [class_caption] * num_class_images
+    def _get_captions(
+        self,
+        num_instance_images, 
+        num_class_images,
+    ) -> Tuple[List, List]:
+        """prepare captions for instance and class images"""
 
-    return instance_captions, class_captions
+        instance_caption = f"a photo of {self.unique_id} {self.class_category}"
+        instance_captions = [instance_caption] * num_instance_images
 
+        class_caption = f"a photo of {self.class_category}"
+        class_captions = [class_caption] * num_class_images
 
-def get_embedded_caption(
-    num_instance_images, num_class_images,
-    unique_id, class_category
-):
-    """get embedded text for each caption with TextEncoder"""
+        return instance_captions, class_captions
 
-    instance_captions, class_captions = get_captions(
-        num_instance_images, num_class_images,
-        unique_id, class_category
-    )
+    def _get_embedded_caption(
+        self,
+        num_instance_images, 
+        num_class_images
+    ) -> np.ndarray:
+        """get embedded text for each caption with TextEncoder"""
 
-    # Collate the tokenized captions into an array.
-    tokenized_texts = np.empty(
-        (num_instance_images + num_class_images, MAX_PROMPT_LENGTH)
-    )
-    for i, caption in enumerate(itertools.chain(instance_captions, class_captions)):
-        tokenized_texts[i] = process_text(caption)
+        instance_captions, class_captions = self._get_captions(
+            num_instance_images, num_class_images,
+            unique_id, class_category
+        )
 
-    # We also pre-compute the text embeddings to save some memory during training.
-    POS_IDS = tf.convert_to_tensor([list(range(MAX_PROMPT_LENGTH))], dtype=tf.int32)
-    text_encoder = TextEncoder(MAX_PROMPT_LENGTH)
+        # Collate the tokenized captions into an array.
+        tokenized_texts = np.empty(
+            (num_instance_images + num_class_images, MAX_PROMPT_LENGTH)
+        )
+        for i, caption in enumerate(itertools.chain(instance_captions, class_captions)):
+            tokenized_texts[i] = self._process_text(caption)
+        
+        embedded_text = self.text_encoder(
+            [tf.convert_to_tensor(tokenized_texts), POS_IDS], training=False
+        ).numpy()
 
-    embedded_text = text_encoder(
-        [tf.convert_to_tensor(tokenized_texts), POS_IDS], training=False
-    ).numpy()
+        return embedded_text
 
-    return embedded_text
+    def _collate_instance_image_paths(
+        self,
+        instance_image_paths, 
+        class_image_paths
+    ) -> List:
+        """make instance_image_paths's length equal to the length of class_image_paths"""
 
-def collate_instance_image_paths(instance_image_paths, class_image_paths):
-    """make instance_image_paths's length equal to the length of class_image_paths"""
+        new_instance_image_paths = []
+        for index in range(len(class_image_paths)):
+            instance_image = instance_image_paths[index % len(instance_image_paths)]
+            new_instance_image_paths.append(instance_image)
 
-    new_instance_image_paths = []
-    for index in range(len(class_image_paths)):
-        instance_image = instance_image_paths[index % len(instance_image_paths)]
-        new_instance_image_paths.append(instance_image)
+        return new_instance_image_paths    
 
-    return new_instance_image_paths    
+    def _download_images(self) -> Tuple[List, List]:
+        """download instance and class images(compressed) from the urls"""
 
-def download_images(
-    instance_images_url,
-    class_images_url
-):
-    """download instance and class images(compressed) from the urls"""
+        instance_images_root = tf.keras.utils.get_file(
+            origin=self.instance_images_url, untar=True,
+        )
+        class_images_root = tf.keras.utils.get_file(
+            origin=self.class_images_url, untar=True,
+        )
 
-    instance_images_root = tf.keras.utils.get_file(
-        origin=instance_images_url, untar=True,
-    )
-    class_images_root = tf.keras.utils.get_file(
-        origin=class_images_url, untar=True,
-    )
+        instance_image_paths = list(paths.list_images(instance_images_root))
+        class_image_paths = list(paths.list_images(class_images_root))
+        instance_image_paths = self._collate_instance_image_paths(
+            instance_image_paths, class_image_paths
+        )
 
-    instance_image_paths = list(paths.list_images(instance_images_root))
-    class_image_paths = list(paths.list_images(class_images_root))
-    instance_image_paths = collate_instance_image_paths(instance_image_paths, class_image_paths)
+        return instance_image_paths, class_image_paths
 
-    return instance_image_paths, class_image_paths
+    def _process_image(
+        self,
+        image_path, 
+        tokenized_text
+    ) -> Tuple[tf.Tensor, tf.Tensor]:
+        """read image file and scale"""
 
-
-def process_image(image_path, tokenized_text):
-    """read image file and scale"""
-
-    image = tf.io.read_file(image_path)
-    image = tf.io.decode_png(image, 3)
-    image = tf.image.resize(image, (RESOLUTION, RESOLUTION))
-    return image, tokenized_text
-
-
-def apply_augmentation(image_batch, embedded_tokens):
-    """apply data augmentation to a batch of images"""
-
-    return augmenter(image_batch), embedded_tokens
-
-
-def prepare_dict(instance_only=True):
-    """
-    get a function that returns a dictionary with an appropriate
-    format for instance and class datasets
-    """
-
-    def fn(image_batch, embedded_tokens):
-        if instance_only:
-            batch_dict = {
-                "instance_images": image_batch,
-                "instance_embedded_texts": embedded_tokens,
-            }
-            return batch_dict
-        else:
-            batch_dict = {
-                "class_images": image_batch,
-                "class_embedded_texts": embedded_tokens,
-            }
-            return batch_dict
-
-    return fn
+        image = tf.io.read_file(image_path)
+        image = tf.io.decode_png(image, 3)
+        image = tf.image.resize(image, (self.img_height, self.img_width))
+        return image, tokenized_text
 
 
-def assemble_dataset(image_paths, embedded_texts, instance_only=True, batch_size=1):
-    dataset = tf.data.Dataset.from_tensor_slices((image_paths, embedded_texts))
-    dataset = dataset.map(process_image, num_parallel_calls=AUTO)
-    dataset = dataset.shuffle(5, reshuffle_each_iteration=True)
-    dataset = dataset.batch(batch_size)
-    dataset = dataset.map(apply_augmentation, num_parallel_calls=AUTO)
+    def _apply_augmentation(
+        self, 
+        image_batch, 
+        embedded_tokens
+    ) -> Tuple[tf.Tensor, tf.Tensor]:
+        """apply data augmentation to a batch of images"""
 
-    prepare_dict_fn = prepare_dict(instance_only=instance_only)
-    dataset = dataset.map(prepare_dict_fn, num_parallel_calls=AUTO)
-    return dataset
+        return self.augmenter(image_batch), embedded_tokens
 
 
-def prepare_datasets(
-    instance_images_url, class_images_url,
-    unique_id, class_category
-):
-    """get tf.data of zipped instance and class datasets"""
+    def _prepare_dict(self, instance_only=True) -> Callable
+        """
+        get a function that returns a dictionary with an appropriate
+        format for instance and class datasets
+        """
 
-    print("downloading instance and class images")
-    instance_image_paths, class_image_paths = download_images(
-        instance_images_url, class_images_url
-    )
+        def fn(image_batch, embedded_tokens) -> Dict[str, tf.Tensor]::
+            if instance_only:
+                batch_dict = {
+                    "instance_images": image_batch,
+                    "instance_embedded_texts": embedded_tokens,
+                }
+                return batch_dict
+            else:
+                batch_dict = {
+                    "class_images": image_batch,
+                    "class_embedded_texts": embedded_tokens,
+                }
+                return batch_dict
 
-    print("preparing embeded caption via TextEncoder")
-    embedded_text = get_embedded_caption(
-        len(instance_image_paths), len(class_image_paths),
-        unique_id, class_category
-    )
+        return fn
 
-    print("assembling instance and class dataset")
-    instance_dataset = assemble_dataset(
-        instance_image_paths,
-        embedded_text[: len(instance_image_paths)],
-    )
-    class_dataset = assemble_dataset(
-        class_image_paths, 
-        embedded_text[len(instance_image_paths) :], 
-        instance_only=False
-    )
 
-    train_dataset = tf.data.Dataset.zip(
-        (instance_dataset, class_dataset)
-    )
-    return train_dataset
-    
+    def _assemble_dataset(
+        self,
+        image_paths, 
+        embedded_texts, 
+        instance_only=True
+    ) -> tf.data.Dataset:
+        dataset = tf.data.Dataset.from_tensor_slices((image_paths, embedded_texts))
+        dataset = dataset.map(self._process_image, num_parallel_calls=AUTO)
+        dataset = dataset.shuffle(5, reshuffle_each_iteration=True)
+        dataset = dataset.batch(self.batch_size)
+        dataset = dataset.map(self._apply_augmentation, num_parallel_calls=AUTO)
+
+        prepare_dict_fn = self._prepare_dict(instance_only=instance_only)
+        dataset = dataset.map(prepare_dict_fn, num_parallel_calls=AUTO)
+        return dataset
+
+
+    def prepare_datasets(self) -> tf.data.Dataset:
+        """get tf.data of zipped instance and class datasets"""
+
+        print("downloading instance and class images")
+        instance_image_paths, class_image_paths = self._download_images()
+
+        print("preparing embeded caption via TextEncoder")
+        embedded_text = self._get_embedded_caption(
+            len(instance_image_paths), len(class_image_paths),
+        )
+
+        print("assembling instance and class dataset")
+        instance_dataset = self._assemble_dataset(
+            instance_image_paths,
+            embedded_text[: len(instance_image_paths)],
+        )
+        class_dataset = self._assemble_dataset(
+            class_image_paths, 
+            embedded_text[len(instance_image_paths) :], 
+            instance_only=False
+        )
+
+        train_dataset = tf.data.Dataset.zip(
+            (instance_dataset, class_dataset)
+        )
+        return train_dataset
