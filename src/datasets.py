437b447bd659f2163c37ae6f03ed7a2d8419bf27
Sayak Paul <spsayakpaul@gmail.com>
2023-01-20 01:11:05 +0100

    fix: shuffle() placement.

--- src/datasets.py
+++ src/datasets.py
@@ -1,5 +1,4 @@
 import itertools
-import random
 from typing import Callable, Dict, List, Tuple
 
 import keras_cv
@@ -120,7 +119,6 @@ class DatasetUtils:
         new_instance_image_paths = []
         num_class_image_paths = len(class_image_paths)
         indices = list(range(num_class_image_paths))
-        random.shuffle(indices)
 
         for index in indices:
             instance_image = instance_image_paths[index % len(instance_image_paths)]
@@ -193,8 +191,8 @@ class DatasetUtils:
         """Assembles `tf.data.Dataset` object from image paths and their corresponding
         captions. `texts` can either be tokens or embedded tokens."""
         dataset = tf.data.Dataset.from_tensor_slices((image_paths, texts))
-        dataset = dataset.map(self._process_image, num_parallel_calls=AUTO)
         dataset = dataset.shuffle(5, reshuffle_each_iteration=True)
+        dataset = dataset.map(self._process_image, num_parallel_calls=AUTO)
         dataset = dataset.batch(self.batch_size)
         dataset = dataset.map(self._apply_augmentation, num_parallel_calls=AUTO)
 
